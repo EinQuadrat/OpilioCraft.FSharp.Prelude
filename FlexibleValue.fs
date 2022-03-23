706eb6ec210a2d85b40721fff1b40e88ce3487a6
Carsten Schäfer <carsten.schaefer@mailbox.org>
2022-03-23 08:26:05 +0100

    Added FlexibleValue type.

---
+++ FlexibleValue.fs
@@ -0,0 +1,27 @@
+﻿namespace OpilioCraft.FSharp.Prelude
+
+[<RequireQualifiedAccess>]
+type FlexibleValue =
+    | Boolean of bool
+    | String of string
+    | Numeral of int
+    | Float of float
+    | Decimal of decimal
+    | DateTime of System.DateTime
+    | Date of System.DateOnly
+    | Time of System.TimeOnly
+    | TimeSpan of System.TimeSpan
+
+    with
+        static member inline Wrap (incoming : 'a) : FlexibleValue =
+            match box incoming with
+            | :? bool as boolValue -> Boolean boolValue
+            | :? string as stringValue -> String stringValue
+            | :? int as intValue -> Numeral intValue
+            | :? float as floatValue -> Float floatValue
+            | :? decimal as decimalValue -> Decimal decimalValue
+            | :? System.DateOnly as dateValue -> Date dateValue
+            | :? System.TimeOnly as timeValue -> Time timeValue
+            | :? System.DateTime as datetimeValue -> DateTime datetimeValue
+            | :? System.TimeSpan as timespanValue -> TimeSpan timespanValue
+            | _ -> invalidArg "value" "{nameof<FlexibleValue>} does not support incoming type {incoming.GetType().Name}"
--- UserSettings.fs
+++ UserSettings.fs
@@ -9,9 +9,11 @@ open Microsoft.FSharp.Reflection
 
 // exceptions
 exception IncompleteSetupException of MissingFile:string
-    with override x.Message = $"incomplete user settings; missing file is: {x.MissingFile}"
+    with override x.Message = $"incomplete user settings; missing file: {x.MissingFile}"
 exception InvalidUserSettingsException of File:string * ErrorMessage:string
     with override x.Message = $"invalid user settings; file {x.File} contains error: {x.ErrorMessage}"
+exception IncompatibleVersionException of Type:Type * Expected:Version * Found:Version
+    with override x.Message = $"version of settings file is not supported\n{x.Type.Name}: expected {x.Expected}, found {x.Found}"
 
 // json converters
 type EnumUnionConverter<'T> () =
@@ -35,21 +37,44 @@ type EnumUnionConverter<'T> () =
 module UserSettingsHelper =
     let (-||-) condition exn = if not condition then raise exn
 
-    // load user settings on demand
-    let lazyLoad<'T> jsonFilename jsonOptions =
-        lazy (
-            File.Exists(jsonFilename) -||- IncompleteSetupException(MissingFile = jsonFilename)
+    // load user settings
+    let load<'T> jsonFilename jsonOptions =
+        File.Exists(jsonFilename) -||- IncompleteSetupException(MissingFile = jsonFilename)
 
-            try
-                let settingsAsJson = File.ReadAllText(jsonFilename)
-                JsonSerializer.Deserialize<'T>(settingsAsJson, options = jsonOptions)
-            with
-            | exn -> raise <| InvalidUserSettingsException(File = jsonFilename, ErrorMessage = exn.Message)
-        )
+        try
+            let settingsAsJson = File.ReadAllText(jsonFilename)
+            JsonSerializer.Deserialize<'T>(settingsAsJson, options = jsonOptions)
+        with
+        | exn -> raise <| InvalidUserSettingsException(File = jsonFilename, ErrorMessage = exn.Message)
+
+    // load user settings on demand
+    let lazyLoad<'T> jsonFilename jsonOptions = lazy ( load<'T> jsonFilename jsonOptions )
+
+    // save
+    let save<'T> jsonFile jsonOptions settings =
+        let json = JsonSerializer.Serialize<'T>(settings, options = jsonOptions) in
+        IO.saveGuard jsonFile <| fun uri -> File.WriteAllText(uri, json)
 
 module Verify =
+    [<Literal>]
+    let VersionPropertyName = "Version"
+
+    let tryGetVersion settings =
+        settings.GetType().GetProperty(VersionPropertyName, typeof<Version>)
+        |> Option.ofObj
+        |> Option.bind (fun versionProperty -> versionProperty.GetValue(settings) :?> Version |> Some)
+
     let raiseIfNone jsonFile message =
         Option.orElseWith (fun () -> raise <| InvalidUserSettingsException(File = jsonFile, ErrorMessage = message))
+
+    let isVersion (expectedVersion : Version) settings =
+        tryGetVersion settings
+        |> function
+            | None ->
+                raise <| IncompatibleVersionException(Type = settings.GetType(), Expected = expectedVersion, Found = Version())
+            | Some foundVersion when foundVersion.CompareTo(expectedVersion) <> 0 ->
+                raise <| IncompatibleVersionException(Type = settings.GetType(), Expected = expectedVersion, Found = foundVersion)
+            | _ -> settings
 
     let isValidDirectory dir =
         match System.IO.Directory.Exists(dir) with
