7d82b46a432a7308f4eb1548ae2759bafc6e2bc3
Carsten Schäfer <carsten.schaefer@mailbox.org>
2022-04-02 08:42:30 +0200

    Adjusted FlexibleValue.Wrap() to prevent nested values.

--- FlexibleValue.fs
+++ FlexibleValue.fs
@@ -2,26 +2,41 @@
 
 [<RequireQualifiedAccess>]
 type FlexibleValue =
+    // primitive types
     | Boolean of bool
-    | String of string
     | Numeral of int
     | Float of float
     | Decimal of decimal
-    | DateTime of System.DateTime
+    | String of string
+    // object types
     | Date of System.DateOnly
     | Time of System.TimeOnly
+    | DateTime of System.DateTime
     | TimeSpan of System.TimeSpan
 
     with
         static member inline Wrap (incoming : 'a) : FlexibleValue =
             match box incoming with
+            | :? FlexibleValue as flexValue -> flexValue // prevent nexted values
             | :? bool as boolValue -> Boolean boolValue
-            | :? string as stringValue -> String stringValue
             | :? int as intValue -> Numeral intValue
             | :? float as floatValue -> Float floatValue
             | :? decimal as decimalValue -> Decimal decimalValue
+            | :? string as stringValue -> String stringValue
             | :? System.DateOnly as dateValue -> Date dateValue
             | :? System.TimeOnly as timeValue -> Time timeValue
             | :? System.DateTime as datetimeValue -> DateTime datetimeValue
             | :? System.TimeSpan as timespanValue -> TimeSpan timespanValue
             | _ -> invalidArg "value" $"{nameof(FlexibleValue)} does not support incoming type {incoming.GetType().Name}"
+
+        override x.ToString () =
+            match x with
+            | FlexibleValue.Boolean boolValue -> boolValue.ToString()
+            | FlexibleValue.Numeral numValue -> numValue.ToString()
+            | FlexibleValue.Float numValue -> numValue.ToString()
+            | FlexibleValue.Decimal numValue -> numValue.ToString()
+            | FlexibleValue.String stringValue -> stringValue
+            | FlexibleValue.Date dateValue -> dateValue.ToString("yyyy-MM-dd")
+            | FlexibleValue.Time timeValue -> timeValue.ToString("hh:mm:ss")
+            | FlexibleValue.DateTime datetimeValue -> datetimeValue.ToString("yyyy-MM-ddThh:mm:ss")
+            | FlexibleValue.TimeSpan timespanValue -> timespanValue.ToString("yyyy-MM-ddThh:mm:ss")
