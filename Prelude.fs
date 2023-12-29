cece2b3f7651fc8bb28e094e4d563b8558ba8399
Carsten Schäfer <carsten.schaefer@mailbox.org>
2023-12-29 08:11:12 +0100

    Added tee functions.

--- Prelude.fs
+++ Prelude.fs
@@ -18,8 +18,32 @@ module Prelude =
     let inline ( -||- ) condition exn = if not condition then raise exn
 
     /// <summary>
-    /// Allows to apply a side effect without changing the value
+    /// Allows applying a side effect without changing the value. Same as operator |>!.
     /// </summary>
     /// <param name="x">The value.</param>
     /// <param name="f">The side effect to apply.</param>
-    let inline ( |>! ) x f = ignore(f x); x
+    let inline tee f x = ignore(f x); x
+
+    /// <summary>
+    /// Allows applying a side effect without changing the value. Same as tee.
+    /// </summary>
+    /// <param name="x">The value.</param>
+    /// <param name="f">The side effect to apply.</param>
+    let inline ( |>! ) x f = tee f x
+
+    /// <summary>
+    /// Applys a side effect without changing the value, if the predicate on x is true.
+    /// </summary>
+    /// <param name="p">The predicate.</param>
+    /// <param name="x">The value.</param>
+    /// <param name="f">The side effect to apply.</param>
+    let inline teeP p f x = ignore(if p x then f x) ; x
+
+    /// <summary>
+    /// Applys a side effect only if cond is true. Returns the value without changing it.
+    /// </summary>
+    /// <param name="cond">The condition.</param>
+    /// <param name="x">The value.</param>
+    /// <param name="f">The side effect to apply.</param>
+    let inline teeIf cond f x = teeP (fun _ -> cond = true) f x
+    
